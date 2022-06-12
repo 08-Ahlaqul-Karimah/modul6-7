b409ee60621b6ad423fb1156314b64b5a1c63bf2
08-Ahlaqul-Karimah <ahlaqulkarimah08@gmail.com>
2022-06-12 19:44:45 +0700

    menambahkan folder modul 6-7 paw

---
+++ resources/views/aboutme.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.main')
+@section('content')
+<div class="container">
+    <h1 class="text-primary mt-3 mb-5">About me</h1>
+        <img src="https://pbs.twimg.com/media/FVDDHhgUAAAjLhC?format=jpg&name=4096x4096" alt="Ahlaqul Karimah M"  width="200px" height="250px" class="mx-auto d-block rounded mb-5">
+    <h2 class="text-primary mt-lg-5">
+        Background
+    </h2>
+    <div class="container">
+        Nama saya adalah Ahlaqul Karimah,biasanya saya dipanggil imawa kalo dikampus, saya anak ke-3 dari 5 bersaudara, saya berasal dari lamongan jawa timur
+    </div>
+    <div class="container">
+       saya lahir pada tanggal 8 oktober 2001 pada jam 01:00 malam
+    </div>
+    <div class="container">
+        saya menyukai beberapa aktor dan aktris thailand, akan tetapi saya bukan golongan fujo
+    </div>
+    <div class="container">
+        teknik Informatika adalah prodi yang saya ambil saat SNMPTN, saya mengambil prodi ini dikarenakan saya berharap tidak lolos SNMPTN agar bisa ikut UTBK dikampus lain karena prodi ini memiliki peluang yang sangat terjangkau, namun takdir berkata lain hehe, saya malah lolos dan harus memasuki prodi ini secara sukarela
+    </div>
+    <div class="container">
+       saya menyukai masakan seafood, saya memiliki cita cita sebagai dokter gigi,, namun cita cita itu musnah ketika saya keterima di teknik informatika
+    <h2 class="text-primary mt-lg-5">
+        Suka Duka PAW
+    </h2>
+    <div class="container">
+        Untuk sukanya adalah dimana kita bisa belajar Pemrograman Aplikasi Web dan mengimplementasikan, namun saya ada
+        beberapa hal yang membuat saya berduka
+        karna setiap tugas praktikum deadline tugasnya terlalu cepat
+    </div>
+</div>
+
+@endsection
